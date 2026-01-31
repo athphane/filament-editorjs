869ebc7723e839b7fe75b7e9f02489bb333f1262
Athfan Khaleel <athphane@gmail.com>
2026-02-01 00:06:07 +0500

    escape correctly

--- resources/views/renderers/inline-code.blade.php
+++ resources/views/renderers/inline-code.blade.php
@@ -4,5 +4,7 @@
 
 <div class="my-2">
     <code
-        class="px-1 py-0.5 rounded bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 font-mono text-sm border border-gray-200 dark:border-gray-700">{{ $code }}</code>
+        class="px-1 py-0.5 rounded bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 font-mono text-sm border border-gray-200 dark:border-gray-700">
+{{ $code }}
+</code>
 </div>
