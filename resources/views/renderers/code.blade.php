dfd64b239601caa2c34f592f3d31bc53429bc4ae
Athfan Khaleel <athphane@gmail.com>
2026-03-17 01:14:11 +0500

    feat: code block styling update

--- resources/views/renderers/code.blade.php
+++ resources/views/renderers/code.blade.php
@@ -5,16 +5,18 @@
         $showLanguageLabel = $config['show_language_label'] ?? true;
     @endphp
 
-    @if($showLanguageLabel && !empty($language) && $language !== 'plaintext')
-        <div class="flex items-center justify-between mb-2">
-            <span class="text-xs font-mono text-gray-500 dark:text-gray-400 uppercase">
-                {{ $language }}
-            </span>
-        </div>
-    @endif
+    <div class="rounded-lg border border-gray-400 my-6">
+        @if($showLanguageLabel && !empty($language) && $language !== 'plaintext')
+            <div class="flex items-center justify-between rounded-t-lg bg-sky-400">
+                <span class="ms-2 text-xs font-mono text-gray-500 dark:text-gray-400 uppercase">
+                    {{ $language }}
+                </span>
+            </div>
+        @endif
 
-    <div class="code-block-wrapper my-6">
-        {!! $code !!}
+        <div class="code-block-wrapper">
+            {!! $code !!}
+        </div>
     </div>
 
 </div>
